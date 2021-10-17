b3073ec5275c8791a8e645106ab0bd2d47305eb2
Yogesh-7523 <87252370+Yogesh-7523@users.noreply.github.com>
2021-10-17 09:58:20 +0530

    Code chef snack down 2021 qualifier solution
    This is a cpp solution of code chef Snack down 2021 qualifier round of problem code: LUCKYNUM

---
+++ luckynumber.cpp
@@ -0,0 +1,17 @@
+// codechef
+//Snack down 2021 qualifier
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	while(n--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		if(a==7 || b==7 || c==7){
+			cout<<"YES"<<endl;
+		}
+		else cout<<"NO"<<endl;
+	}
+}
